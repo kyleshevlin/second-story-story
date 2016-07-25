2384adddd297f6d5520d1e068082427228b9a2f1
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2016-07-24 23:09:02 -0700

    First lesson, gotta super the arguments

--- app/router.js
+++ app/router.js
@@ -4,6 +4,7 @@ import config from './config/environment';
 const Router = Ember.Router.extend({
   location: config.locationType,
   didTransition() {
+    this._super(...arguments);
     window.scrollTo(0,0);
   }
 });
